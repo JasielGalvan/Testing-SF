f02ca9cf9d746437bc1109560a3196c52c8a8f33
Jasiel Galvan <jasielgalvan@hotmail.com>
2022-04-24 18:46:24 -0500

    Se optimiza ejercicio ValidatePIN

--- src/main/java/com/solucionfactible/dev/ValidatePIN.java
+++ src/main/java/com/solucionfactible/dev/ValidatePIN.java
@@ -12,13 +12,11 @@ Fecha: 2022-04-24
 public class ValidatePIN {
     public static boolean isValid(String pin) {
         /*Se valida si es numero valido*/
-        if(isNum(pin)){
+        if(isNum(pin))
             /*Si es un numero valido procede a validar si la longitud de la cadena es 4 o 6*/
             return (pin.length() == 4 || pin.length() == 6);
-        }else{
-            /*En caso de no ser un numero valida retorna Falso*/
-            return false;
-        }
+        /*En caso de no ser un numero valida retorna Falso*/
+        return false;
     }
     /*El metodo isNum, verifica si la cadena que recibe es un entero o no.*/
     public static boolean isNum(String num){
