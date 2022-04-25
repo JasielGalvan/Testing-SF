cdf900ca47254c4b183c576d97482fe33aeef296
Jasiel Galvan <jasielgalvan@hotmail.com>
2022-04-24 22:45:53 -0500

    Ejercicio FindMissingLetter

--- src/main/java/com/solucionfactible/dev/FindMissingLetter.java
+++ src/main/java/com/solucionfactible/dev/FindMissingLetter.java
@@ -1,5 +1,10 @@
 package com.solucionfactible.dev;
 
+import java.util.stream.IntStream;
+/*
+Autor: Jasiel Galvan
+Fecha: 2022-04-24
+*/
 /**
  * findMissingLetter takes an array of consecutive (increasing) letters as input and that returns the missing letter 
  * in the array. You will always get an valid array. And it will be always exactly one letter be missing. 
@@ -9,9 +14,13 @@ package com.solucionfactible.dev;
  * @author developer
  */
 public class FindMissingLetter {
-	
-	public static char findMissingLetter(char[] array) {
-		return ' ';
-	}
-
+    public static char findMissingLetter(char[] array) {
+        /*Se usa el codigo ASCII para trabajar con los valores numericos de cada caracter*/
+        char missLetter = IntStream.range(0, array.length - 1)
+        .filter(x -> array[x]+1 != array[x+1])//Se suma 1 al valor del primer elemento y se valida que sea distinto al valor del siguiente
+        .mapToObj(x -> (char)(array[x]+1))//Se mapean los elementos como tipo char
+        .findAny().orElse(' ');//En caso de que no encuentre nada, devuelve ' ' 
+        
+        return missLetter;
+    }
 }
--- src/test/java/com/solucionfactible/dev/FindMissingLetterTest.java
+++ src/test/java/com/solucionfactible/dev/FindMissingLetterTest.java
@@ -1,20 +1,16 @@
 package com.solucionfactible.dev;
 
-import static org.junit.Assert.*;
-
 import org.junit.Test;
-
+import static org.junit.Assert.*;
 import static com.solucionfactible.dev.FindMissingLetter.findMissingLetter;
 
 public class FindMissingLetterTest {
-
-	@Test
-	public void test() {
-		assertEquals('e', findMissingLetter(new char[] { 'a','b','c','d','f' }));
-		assertEquals('m', findMissingLetter(new char[] { 'k','l','n','o' }));
-		assertEquals('w', findMissingLetter(new char[] { 'u','v','x' }));
+    @Test
+    public void test() {
+        assertEquals('e', findMissingLetter(new char[] { 'a','b','c','d','f' }));
+	assertEquals('m', findMissingLetter(new char[] { 'k','l','n','o' }));
+	assertEquals('w', findMissingLetter(new char[] { 'u','v','x' }));
         assertEquals('P', findMissingLetter(new char[] { 'O','Q','R','S' }));
         assertEquals('Y', findMissingLetter(new char[] { 'X','Z' }));
-	}
-
+    }
 }
